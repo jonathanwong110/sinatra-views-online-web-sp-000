ae84e22162525cccf70d9599830f9dbaee762d1c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-15 00:46:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-    "<h1>Hello World</h1>"
-	end
-	
-  get '/' do
     erb :index
-  end
+	end
 
   get "/info" do
     erb :dogs
